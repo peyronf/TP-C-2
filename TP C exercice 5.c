c20f334e5cc164b358f7e1f3aaaf86982c6842fd
Florian PEYRON <florian.peyron@hotmail.fr>
2013-10-21 17:42:08 +0200

    Exercice 5 réalisé

--- TP C exercice 5.c
+++ TP C exercice 5.c
@@ -8,4 +8,19 @@ void main()
     scanf("%f", &meilleure_note_2);
     printf("\nSaisir la troisieme meilleure note : ");
     scanf("%f", &meilleure_note_3);
+    if (meilleure_note_1<=meilleure_note_2 && meilleure_note_1<= meilleure_note_3)
+    {
+        printf("La moyenne est de : %f", (meilleure_note_2+meilleure_note_3)/2);
+    }
+    else
+
+    {
+        if (meilleure_note_2<=meilleure_note_1 && meilleure_note_2<=meilleure_note_3)
+        {
+            printf("La moyenne est de : %f", (meilleure_note_1+meilleure_note_3)/2);
+        }
+        else
+            printf("La moyenne est de : %f", (meilleure_note_1+meilleure_note_2)/2);
+
+    }
 }
