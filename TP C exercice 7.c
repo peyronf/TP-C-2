215f3ab01f44aae11174d3dca19acb8c5619ac9e
Florian PEYRON <florian.peyron@hotmail.fr>
2013-10-21 17:59:50 +0200

    Exercice 7 terminé

--- TP C exercice 7.c
+++ TP C exercice 7.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+void main()
+{
+    float a, b, c, delta, x1, x2, x3;
+    printf("Saisir a : ");
+    scanf("%f", &a);
+    printf("\nSaisir b : ", &b);
+    scanf("%f", &b);
+    printf("\nSaisir c : ", &c);
+    scanf("%f", c);
+    delta=b*b-4*a*c;
+    printf("Ainsi Delta = %f", delta);
+    if (delta>0)
+    {
+        x1=(-b-sqrt(delta))/(2*a);
+        x2=(-b+sqrt(delta))/(2*a);
+        printf("Les solutions sont : x = %f ou x = %f", x1, x2);
+    }
+    else if (delta=0)
+    {
+        x3=(-b)/(2*a);
+        printf("La seule solution est : x = %f", x3);
+    }
+}
