e8d8b45fa1231295c10c4a843563f62aff549c73
Florian PEYRON <florian.peyron@hotmail.fr>
2013-10-22 10:12:09 +0200

    Exercice 6 terminé
    Merci Alexandre =p

--- TP C exercice 6.c
+++ TP C exercice 6.c
@@ -1,1 +1,40 @@
-
+#include <stdio.h>
+void main()
+{
+    char chiffre;
+    printf("choisir un chiffre :");
+    scanf(" %c",&chiffre);
+    switch(chiffre)
+    {
+    case '0':
+        printf("ZERO");
+        break;
+    case '1':
+        printf("UN");
+        break;
+    case '2':
+        printf("DEUX");
+        break;
+    case '3':
+        printf("TROIS");
+        break;
+    case '4':
+        printf("QUATRE");
+        break;
+    case '5':
+        printf("CINQ");
+        break;
+    case '6':
+        printf("SIX");
+        break;
+    case '7':
+        printf("SEPT");
+        break;
+    case '8':
+        printf("HUIT");
+        break;
+    case '9':
+        printf("NEUF");
+        break;
+    }
+}
