14f705f90c677d43a07a016ebc15c037dbc8fef7
Florian PEYRON <florian.peyron@hotmail.fr>
2013-10-22 10:44:32 +0200

    Exercice 8 (à compléter)

--- TP C exercice 8.c
+++ TP C exercice 8.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 void main()
 {
-    char mois
+    char mois;
+    int jour,annee;
+    printf("Indiquer la date en format jjmmaa : ");
+    scanf("%d, %c,%d", jour, mois, annee);
+    /* utiliser un switch case pour le mois, et revoyer les valeurs
+    entière pour le jour et l'annee */
 }
